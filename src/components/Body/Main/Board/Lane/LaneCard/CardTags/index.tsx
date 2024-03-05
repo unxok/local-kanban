b5c6541858bbb19d5c582a02c42f155074b0b872
unxok <54017499+unxok@users.noreply.github.com>
2024-03-05 13:18:32 -0800

    Update index.tsx

--- src/components/Body/Main/Board/Lane/LaneCard/CardTags/index.tsx
+++ src/components/Body/Main/Board/Lane/LaneCard/CardTags/index.tsx
@@ -1,7 +1,7 @@
 import { Badge } from "@/components/ui/badge";
 
 export const CardTags = ({ tags }: { tags: string[] }) => (
-  <div className="line-clamp-1 flex flex-row items-center justify-start gap-1">
+  <div className="flex flex-row flex-wrap items-center justify-start gap-1">
     {tags.map((t) => (
       <Badge className="font-normal" key={t}>
         {t}
